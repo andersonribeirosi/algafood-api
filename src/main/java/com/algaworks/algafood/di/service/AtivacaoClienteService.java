fa0ad2b560826a48723bf8b01856e5c4dc16e5ca
Anderson <andersonribeiro.si@hotmail.com>
2020-05-04 18:18:21 -0300

    Desambinguação de beans com anotações customizadas

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -4,17 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
 @Component
 public class AtivacaoClienteService {
 
-	@Autowired
+//	@Autowired(required = false)
+//	private List<Notificador> notificadores;
+
+//	for (Notificador notificador : notificadores) {
+//		notificador.notificar(cliente, "Seu cadastro esta ativo");
+//	}
+
+//	@Qualifier("urgente")
+	@TipoDoNotificador(NivelUrgencia.NORMAL)
+	@Autowired(required = false)
 	private Notificador notificador;
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		notificador.notificar(cliente, "Seu cadastro esta ativo");
+		if (notificador != null) {
+			notificador.notificar(cliente, "Seu cadastro esta ativo");
+		} else {
+			System.out.println("Não existe notificador, mas o cliente foi ativado");
+		}
 	}
 }
